d5469b9d95ecea6fbcd487d124470194c16e60ec
李明强 <438562332@qq.com>
2023-08-18 16:34:23 +0800

    修正额外字典处理

--- minimalist-common/src/main/java/com/minimalist/common/extraDict/ExtraDictHandler.java
+++ minimalist-common/src/main/java/com/minimalist/common/extraDict/ExtraDictHandler.java
@@ -5,9 +5,8 @@ import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.minimalist.common.entity.BeanMethod;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.BeansException;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import java.lang.reflect.Method;
@@ -63,21 +62,34 @@ public class ExtraDictHandler {
         Method[] proxyMethods = bean.getClass().getDeclaredMethods();
         //Spring代理类中的方法转Map，key：方法名，value：代理方法
         Map<String, Method> proxyMethodMap = Arrays.stream(proxyMethods).collect(Collectors.toMap(Method::getName, Function.identity(), (v1, v2) -> v1));
-        //获取Spring代理类，再获取代理目标类
-        Class<?> superclass = bean.getClass().getSuperclass();
-        //获取代理目标类中的方法，目的是为了获取到 ExtraDict 自定义注解
-        Method[] methods = superclass.getDeclaredMethods();
-        for (Method method : methods) {
-            //获取自定义注解
-            ExtraDict extraDict = method.getDeclaredAnnotation(ExtraDict.class);
-            if (ObjectUtil.isNotNull(extraDict) && StrUtil.isNotBlank(extraDict.dictType())) {
-                //从代理方法中查找并缓存代理方法，如果代理方法不存在，则缓存代理目标类方法
-                Method proxyMethod = proxyMethodMap.get(method.getName());
-                if (ObjectUtil.isNotNull(proxyMethod)) {
-                    dictMethodMap.put(extraDict.dictType(), new BeanMethod<>(bean, proxyMethod));
-                } else {
-                    dictMethodMap.put(extraDict.dictType(), new BeanMethod<>(bean, method));
-                }
+        //判断是否是cglib动态代理
+        boolean cglibProxy = AopUtils.isCglibProxy(bean);
+        if (cglibProxy) {
+            //获取Spring代理类，再获取代理目标类
+            Class<?> superclass = bean.getClass().getSuperclass();
+            //获取代理目标类中的方法，目的是为了获取到 ExtraDict 自定义注解
+            Method[] methods = superclass.getDeclaredMethods();
+            for (Method method : methods) {
+                cacheBeanMethod(method, proxyMethodMap, bean);
+            }
+        } else {
+            //不是cglib动态代理，可能是其他代理，暂时还没遇到其他代理扫描不到的情况
+            for (Method method : proxyMethods) {
+                cacheBeanMethod(method, proxyMethodMap, bean);
+            }
+        }
+    }
+
+    private void cacheBeanMethod(Method method, Map<String, Method> proxyMethodMap, Object bean) {
+        //获取自定义注解
+        ExtraDict extraDict = method.getDeclaredAnnotation(ExtraDict.class);
+        if (ObjectUtil.isNotNull(extraDict) && StrUtil.isNotBlank(extraDict.dictType())) {
+            //从代理方法中查找并缓存代理方法，如果代理方法不存在，则缓存代理目标类方法
+            Method proxyMethod = proxyMethodMap.get(method.getName());
+            if (ObjectUtil.isNotNull(proxyMethod)) {
+                dictMethodMap.put(extraDict.dictType(), new BeanMethod<>(bean, proxyMethod));
+            } else {
+                dictMethodMap.put(extraDict.dictType(), new BeanMethod<>(bean, method));
             }
         }
     }
