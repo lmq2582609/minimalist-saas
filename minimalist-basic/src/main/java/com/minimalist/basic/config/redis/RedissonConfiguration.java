19519db50a3ee0ac08a693c4245eb9e6da62eda8
小太阳 <438562332@qq.com>
2024-11-15 19:57:37 +0800

    redis无密码配置修正

--- minimalist-basic/src/main/java/com/minimalist/basic/config/redis/RedissonConfiguration.java
+++ minimalist-basic/src/main/java/com/minimalist/basic/config/redis/RedissonConfiguration.java
@@ -1,11 +1,13 @@
 package com.minimalist.basic.config.redis;
 
+import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,14 +32,16 @@ public class RedissonConfiguration {
         Integer connectionPoolSize = environment.getProperty("redisson.connectionPoolSize", Integer.class);
         Integer connectionMinimumIdleSize = environment.getProperty("redisson.connectionMinimumIdleSize", Integer.class);
         Integer database = environment.getProperty("redisson.database", Integer.class);
-        config.useSingleServer()
+        SingleServerConfig singleServerConfig = config.useSingleServer()
                 .setAddress(address)
-                .setPassword(password)
                 .setKeepAlive(true)
                 .setDatabase(database)
                 .setConnectionPoolSize(connectionPoolSize)
                 .setConnectionMinimumIdleSize(connectionMinimumIdleSize)
                 .setPingConnectionInterval(pingInterval);
+        if (StrUtil.isNotBlank(password)) {
+            singleServerConfig.setPassword(password);
+        }
         //设置redisson序列化与反序列化，jackson增加java时间模块处理
         JsonJacksonCodec jsonJacksonCodec = new JsonJacksonCodec();
         ObjectMapper objectMapper = jsonJacksonCodec.getObjectMapper();
